cd4723c0ac8be8a87957fecabbc78f0b120bbf66
Raghu Betina <raghu@firstdraft.com>
2019-03-07 20:45:44 +0000

    Whitelist columns in Building for ActiveAdmin

--- app/admin/buildings.rb
+++ app/admin/buildings.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Building do
+
+ permit_params :apartment_id, :address, :neighborhood, :contact_info
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
