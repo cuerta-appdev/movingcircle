6912ea1fe097a85974fd06ef66d795e85f2477fa
Raghu Betina <raghu@firstdraft.com>
2019-03-07 20:43:12 +0000

    Generate apartments resource with draft:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  # Routes for the Apartment resource:
+
+  # CREATE
+  get("/apartments/new", { :controller => "apartments", :action => "new_form" })
+  post("/create_apartment", { :controller => "apartments", :action => "create_row" })
+
+  # READ
+  get("/apartments", { :controller => "apartments", :action => "index" })
+  get("/apartments/:id_to_display", { :controller => "apartments", :action => "show" })
+
+  # UPDATE
+  get("/apartments/:prefill_with_id/edit", { :controller => "apartments", :action => "edit_form" })
+  post("/update_apartment/:id_to_modify", { :controller => "apartments", :action => "update_row" })
+
+  # DELETE
+  get("/delete_apartment/:id_to_remove", { :controller => "apartments", :action => "destroy_row" })
+
+  #------------------------------
+
   devise_for :users
   # Routes for the User resource:
 
