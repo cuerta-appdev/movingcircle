a3297b17db5bbfe554ca2ad3f62d5b0d7474a749
Raghu Betina <raghu@firstdraft.com>
2019-03-07 20:46:08 +0000

    Whitelist columns in FurnitureCategoryPicture for ActiveAdmin

--- app/admin/furniture_category_pictures.rb
+++ app/admin/furniture_category_pictures.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register FurnitureCategoryPicture do
+
+ permit_params :category_id, :picture
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
