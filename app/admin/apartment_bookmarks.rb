c0cda01a15980bb3a2c1d84ecb564fa21a243263
Raghu Betina <raghu@firstdraft.com>
2019-03-07 20:45:23 +0000

    Whitelist columns in ApartmentBookmark for ActiveAdmin

--- app/admin/apartment_bookmarks.rb
+++ app/admin/apartment_bookmarks.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register ApartmentBookmark do
+
+ permit_params :user_id, :apartment_id, :name
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
