1ba6bed5d85c0d9c4ad8877a1d957a86814d78ce
Raghu Betina <raghu@firstdraft.com>
2019-03-07 20:45:50 +0000

    Whitelist columns in BuildingAmenity for ActiveAdmin

--- app/admin/building_amenities.rb
+++ app/admin/building_amenities.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register BuildingAmenity do
+
+ permit_params :building_id, :amenity_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
