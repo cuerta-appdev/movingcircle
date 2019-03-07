d7f6e49ae0cf38ddcf94f1264dd923e830c0c20f
Raghu Betina <raghu@firstdraft.com>
2019-03-07 20:45:56 +0000

    Whitelist columns in BuildingBookmark for ActiveAdmin

--- app/admin/building_bookmarks.rb
+++ app/admin/building_bookmarks.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register BuildingBookmark do
+
+ permit_params :building_id, :user_id, :name
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
