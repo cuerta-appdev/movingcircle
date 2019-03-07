b4282cca14e93661d377e8c50a07e080e4d0d461
Raghu Betina <raghu@firstdraft.com>
2019-03-07 20:44:53 +0000

    Whitelist columns in FurtnitureObject for ActiveAdmin

--- app/admin/furtniture_objects.rb
+++ app/admin/furtniture_objects.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register FurtnitureObject do
+
+ permit_params :price, :description, :furniture_pack_id, :category_id, :name
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
