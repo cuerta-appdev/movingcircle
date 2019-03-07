6a4fe3d5d2b5dd7d0b703327fbad695932ba61a0
Raghu Betina <raghu@firstdraft.com>
2019-03-07 20:45:05 +0000

    Whitelist columns in ApartmentPhoto for ActiveAdmin

--- app/admin/apartment_photos.rb
+++ app/admin/apartment_photos.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register ApartmentPhoto do
+
+ permit_params :description, :apartment_id, :photo
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
