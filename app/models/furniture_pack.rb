7ba0613905390d29af1f3e21425ec5d27f119efc
Raghu Betina <raghu@firstdraft.com>
2019-03-07 20:44:15 +0000

    Direct association between furniture_packs and furtniture_objects

--- app/models/furniture_pack.rb
+++ app/models/furniture_pack.rb
@@ -1,6 +1,9 @@
 class FurniturePack < ApplicationRecord
   # Direct associations
 
+  has_many   :furtniture_objects,
+             :dependent => :destroy
+
   belongs_to :user
 
   # Indirect associations
--- app/models/furtniture_object.rb
+++ app/models/furtniture_object.rb
@@ -1,6 +1,8 @@
 class FurtnitureObject < ApplicationRecord
   # Direct associations
 
+  belongs_to :furniture_pack
+
   has_many   :furniture_pictures,
              :class_name => "FurnitureObjectPicture",
              :foreign_key => "furniture_object_id",
