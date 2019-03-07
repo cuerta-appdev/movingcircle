c87391fe3b0575b9c0adc4c121d17c20a25bd04c
Raghu Betina <raghu@firstdraft.com>
2019-03-07 20:48:01 +0000

    Only furniture_pack user can modify furniture_pack

--- app/controllers/furniture_packs_controller.rb
+++ app/controllers/furniture_packs_controller.rb
@@ -1,4 +1,14 @@
 class FurniturePacksController < ApplicationController
+  before_action :current_user_must_be_furniture_pack_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_furniture_pack_user
+    furniture_pack = FurniturePack.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == furniture_pack.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @furniture_packs = FurniturePack.all
 
