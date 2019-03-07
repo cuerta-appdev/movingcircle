e12d8fbdbf6a4c941b6357d333a497a0378dbb20
Raghu Betina <raghu@firstdraft.com>
2019-03-07 20:45:37 +0000

    Whitelist columns in AmenityPicture for ActiveAdmin

--- app/admin/amenity_pictures.rb
+++ app/admin/amenity_pictures.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register AmenityPicture do
+
+ permit_params :picture, :description, :building_amenity_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
